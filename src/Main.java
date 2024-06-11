881a0386fd780ce0d40b370c33fb967b6fb443af
emilmehdiyev <emil.mehdiyev76@gmail.com>
2024-06-11 11:38:34 +0400

    Initial2

--- src/Main.java
+++ src/Main.java
@@ -41,19 +41,19 @@ public class Main {
 
         // TASK5
 
-        int month = 7;
+        int month = 13;
 
         switch (month) {
-            case 1:
+            case 1: case 2: case 3:
                 System.out.println("Winter");
                 break;
-            case 4:
+            case 4: case 5: case 6:
                 System.out.println("Spring");
                 break;
-            case 7:
+            case 7: case 8: case 9:
                 System.out.println("Summer");
                 break;
-            case 10:
+            case 10: case 11: case 12:
                 System.out.println("Autumn");
                 break;
             default:
